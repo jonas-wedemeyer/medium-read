01c1d23733da3b00998b97bdd78637cea0abdde5
Jonas <jo.wedemeyer@gmail.com>
2019-07-28 13:27:01 +0200

    feat(dashboard): simple react app

---
+++ client/dashboard/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from React; 
+import './App.js'; 
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Hi, I am the App!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
