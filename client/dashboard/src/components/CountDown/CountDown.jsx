47f2b8a3b48d45c025dffdc4c983e91efb4aad69
Jonas <jo.wedemeyer@gmail.com>
2019-07-29 10:03:11 +0200

    feat(dashboard): starting to work on countdown component

---
+++ client/dashboard/src/components/CountDown/CountDown.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import Moment from 'react-moment';
+
+export default function CountDown({ date_added }) {
+  const [timer, setTimer] = useState({
+    days: null,
+    hours: null,
+    minutes: null,
+    seconds: null,
+  });
+
+  const refreshTimer = (date) => {
+ 
+  }
+
+  return (
+    <div>
+
+    </div>
+  )
+}
