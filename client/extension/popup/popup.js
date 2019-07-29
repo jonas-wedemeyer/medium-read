8633f7b2ceefc1af90677e21ed6d5637b3835a7f
Jonas <jo.wedemeyer@gmail.com>
2019-07-29 08:55:47 +0200

    feat(extension): connected extension to server

--- client/extension/popup/popup.js
+++ client/extension/popup/popup.js
@@ -4,3 +4,29 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     document.getElementById('title').innerHTML = res.title;
   });
 });
+
+// Post an article
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('article-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { getArticle: true }, ({ title, description, image, url }) => { // eslint-disable-line max-len
+        fetch('http://localhost:4000/articles', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            title,
+            description,
+            image,
+            url,
+          }),
+        })
+          .then(res => res.json())
+          .then(article => console.log(article)) // Will be used to display success message
+          .catch(err => console.log(err.message));
+      });
+    });
+  });
+});
