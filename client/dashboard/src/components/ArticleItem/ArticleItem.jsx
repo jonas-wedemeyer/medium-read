46d4b20fe244e83348c07497beb3f0aaca2eaccd
Jonas <jo.wedemeyer@gmail.com>
2019-07-30 15:34:48 +0200

    refactor(dashboard): improve rendering of image by using a div instead of canvas tag

--- client/dashboard/src/components/ArticleItem/ArticleItem.jsx
+++ client/dashboard/src/components/ArticleItem/ArticleItem.jsx
@@ -15,28 +15,19 @@ const InfoBox = styled.div`
   width: 750px;
 `;
 
+const Poster = styled.div`
+  background: grey;
+  height: 50px;
+  width: 750px;
+  background-position: center;
+`;
+
 export default function ArticleItem({ article, deleteOne }) {
-  const canvasRef = useRef(null);
-
-  const renderImage = (imageUrl) => {
-    const img = new Image();
-    img.src = imageUrl;
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 226, 233, canvas.clientWidth, canvas.clientHeight, 0, 0, canvas.clientWidth, canvas.clientHeight); // eslint-disable-line max-len
-  };
-
-  useEffect(() => {
-    renderImage(article.image);
-  });
-
   return (
     <div style={{ marginBottom: '15px' }}>
       <a href={article.url} rel="noopener noreferrer" target="_blank" style={{ textDecoration: 'none', 'a:visited': 'black' }}>
         <FlexColDiv>
-          <canvas width="750px" height="50px" ref={canvasRef} style={{ position: 'relative' }}>
-            <img src={article.image} alt="hi" />
-          </canvas>
+        <Poster style={{ backgroundImage: `url(${article.image})` }} />
           <h4>
             {article.title}
           </h4>
