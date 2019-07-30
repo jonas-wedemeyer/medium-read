b4f1e7ebb0872044e197b9f9fda78ca7b456a1de
Jonas <jo.wedemeyer@gmail.com>
2019-07-30 22:54:06 +0200

    feat(extension): added pages for different contexts & handling if user on medium.com

--- client/extension/popup/popup.js
+++ client/extension/popup/popup.js
@@ -1,12 +1,30 @@
-// Request article information from contentscript & inject it into the popup.html
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  chrome.tabs.sendMessage(tabs[0].id, { getArticle: true }, (res) => {
-    document.getElementById('title').innerHTML = res.title;
+const getCurrDomain = () => {
+  return new Promise((res) => {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      res(tabs[0].url);
+    });
   });
-});
+};
 
-// Post an article
-document.addEventListener('DOMContentLoaded', () => {
+getCurrDomain()
+    .then((domain) => domain
+        .match(/:\/\/\w+/)
+        .toString()
+        .includes('medium')
+            ? articleInfo() && postArticle()
+            : displayNotOnMedium());
+
+// Request article information from contentscript & inject it
+const articleInfo = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { getArticle: true }, (res) => {
+      document.getElementById('title').innerHTML = res.title;
+    });
+  });
+};
+
+// Post an article to the server
+const postArticle = () => {
   document.getElementById('article-form').addEventListener('submit', (e) => {
     e.preventDefault();
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -14,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
         fetch('http://localhost:4000/articles', {
           method: 'POST',
           headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify({
             title,
@@ -23,10 +41,22 @@ document.addEventListener('DOMContentLoaded', () => {
             url,
           }),
         })
-          .then(res => res.json())
-          .then(article => console.log(article)) // Will be used to display success message
-          .catch(err => console.log(err.message));
+            .then(res => res.status === 201 ? res.json() : console.log('Expected another HTTP status.')) // eslint-disable-line
+            .then(() => displaySucessPage()) // TODO: Add title to succes page
+            .catch((err) => console.log(err.message)); // TODO: Show an error page
       });
     });
   });
-});
+};
+
+// Function for for showing the success page
+const displaySucessPage = () => {
+  document.getElementById('article-form').classList.add('hidden');
+  document.getElementById('success-page').classList.remove('hidden');
+};
+
+// Function for displaying the pop for not being on Medium
+const displayNotOnMedium = () => {
+  document.getElementById('article-form').classList.add('hidden');
+  document.getElementById('not-on-medium').classList.remove('hidden');
+};
