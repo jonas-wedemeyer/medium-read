bd54709b9c5a55f3db946ef53c9c4e6b80208b67
Jonas Wedemeyer <jonas@Jonass-MBP.lan>
2019-08-27 18:56:06 +0200

    build(dashboard): generate dist file directly in extension folder

--- client/dashboard/webpack.config.js
+++ client/dashboard/webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.jsx',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(`${__dirname}/../extension`, 'dist'),
     filename: 'main.js',
   },
   module: {
